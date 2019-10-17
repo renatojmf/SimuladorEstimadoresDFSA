1f754998a5d54b063fe9e1454e30cca554d42793
JosenildoV <jva@cin.ufpe.br>
2019-10-17 18:16:16 -0300

    Separação dos graficos

--- grafico.js
+++ grafico.js
@@ -1,9 +1,14 @@
 
 
 function rodarGrafico(){
-    var ctx = document.getElementById('lineColision').getContext('2d');
+    var ctxTotais = document.getElementById('graficoSlotsTotais').getContext('2d');
+    var ctxVazios = document.getElementById('graficoSlotsVazios').getContext('2d');
+    var ctxColisoes = document.getElementById('graficoSlotsColisoes').getContext('2d');
+    var ctxEficiencia = document.getElementById('graficoSlotsEficiencia').getContext('2d');
+    var ctxEstimador = document.getElementById('graficoSlotsEstimador').getContext('2d');
+    var ctxSimulador = document.getElementById('graficoSlotsSimulador').getContext('2d');
 
-    var chart = new Chart(ctx, {
+    var grafico_slots_totais = new Chart(ctxTotais, {
         // The type of chart we want to create
         type: 'line',
     
@@ -15,23 +20,91 @@ function rodarGrafico(){
                 borderColor: 'rgb(0,0,0)',
                 fill: false,
                 data: media['slots']
-            },{
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+
+    var grafico_slots_vazios = new Chart(ctxVazios, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: media['etiquetas'],
+            datasets: [{
                 label: 'Slots vazios totais',
                 borderColor: 'rgb(255,0,0)',
                 fill: false,
                 data: media['vazio']
-            },{
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+
+    var grafico_slots_colisoes = new Chart(ctxColisoes, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: media['etiquetas'],
+            datasets: [{
                 label: 'Colisões totais',
                 borderColor: 'rgb(0,255,0)',
                 fill: false,
                 data: media['colisao']
-            },{
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+
+    var grafico_slots_eficiencia = new Chart(ctxEficiencia, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: media['etiquetas'],
+            datasets: [{
                 label: 'Eficiência',
                 borderColor: 'rgb(0,0,255)',
                 fill: false,
                 data: media['eficiencia'].concat([0,100])
-            },{
-                label: 'Tempo médio de execução',
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+
+    var grafico_slots_tempo_estimador = new Chart(ctxEstimador, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: media['etiquetas'],
+            datasets: [{
+                label: 'Tempo médio de execução do estimador',
                 borderColor: 'rgb(0,255,255)',
                 fill: false,
                 data: media['tempo']
@@ -39,6 +112,32 @@ function rodarGrafico(){
         },
     
         // Configuration options go here
-        options: {}
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
     });
+
+    var grafico_slots_tempo_Simulador = new Chart(ctxSimulador, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: media['etiquetas'],
+            datasets: [{
+                label: 'Tempo médio de execução do simulador',
+                borderColor: 'rgb(0,255,255)',
+                fill: false,
+                data: media['tempo']
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+    
 }
